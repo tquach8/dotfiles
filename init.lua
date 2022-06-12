ba00112836238074d604a4261166503dd5846b25
tquach8 <tan.quach@clio.com>
2022-06-11 23:13:49 -0600

    clean up unused commands

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require "mappings"
 require "settings"
 require "plugin.treesitter"
 require "plugin.lsp"
-require "plugin.toggleterm"
 
 local function clone_paq()
   local path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
@@ -21,7 +20,7 @@ clone_paq()
 vim.cmd('packadd paq-nvim')
 require 'paq' {
   "savq/paq-nvim";
-  
+
   -- LSP plugins
   "nvim-treesitter/nvim-treesitter";
   "neovim/nvim-lspconfig";
@@ -29,8 +28,8 @@ require 'paq' {
   "hrsh7th/cmp-nvim-lsp";
   "L3MON4D3/LuaSnip";
   "saadparwaiz1/cmp_luasnip";
-  "vim-test/vim-test";
-  "akinsho/toggleterm.nvim";
+  "APZelos/blamer.nvim";
+  "tpope/vim-fugitive";
 
   "junegunn/fzf";
   "junegunn/fzf.vim";
@@ -44,8 +43,11 @@ require 'paq' {
 
   "lukas-reineke/indent-blankline.nvim";
   "jiangmiao/auto-pairs";
-  -- 'akinsho/bufferline.nvim';
-  -- 'kyazdani42/nvim-web-devicons';
+  "vim-airline/vim-airline";
+  "vim-airline/vim-airline-themes";
+
+  "preservim/vimux";
+  "pgr0ss/vimux-ruby-test"
 }
 vim.g.vscode_style = "dark"
 vim.g.vscode_italic_comment = 1
--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -22,6 +22,7 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+require'lspconfig'.gopls.setup{}
 
 local cmp = require 'cmp'
 cmp.setup {
@@ -34,7 +35,7 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
