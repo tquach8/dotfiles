ec4d4afb69ab8c81f243f676958bba460e48f37e
tquach8 <tanquach08@gmail.com>
2023-02-09 19:03:41 -0700

    Remove paq loader

--- init.lua
+++ init.lua
@@ -4,21 +4,6 @@ require "plugin.treesitter"
 require "plugin.lsp"
 require("plugin.tokyonight")
 
-local function clone_paq()
-  local path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-  if vim.fn.empty(vim.fn.glob(path)) > 0 then
-    vim.fn.system {
-      'git',
-      'clone',
-      '--depth=1',
-      'https://github.com/savq/paq-nvim.git',
-      path
-    }
-  end
-end
-
-clone_paq()
-vim.cmd('packadd paq-nvim')
 require 'paq' {
   "savq/paq-nvim";
 
