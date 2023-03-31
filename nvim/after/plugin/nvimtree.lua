f0007b2a216c9504eadcb3151b2e94ea39adf58b
tquach8 <tanquach08@gmail.com>
2023-03-31 01:16:59 -0600

    Start nvim tree on setup

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -4,19 +4,10 @@ vim.g.loaded_netrwPlugin = 1
 vim.keymap.set('n', '<C-t>', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>ff', ':NvimTreeFindFile<CR>')
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- empty setup using defaults
 require("nvim-tree").setup()
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
