da7709f1bac8da9cda87ae5935b3272f252df8fb
tquach8 <tanquach08@gmail.com>
2023-10-10 09:13:29 -0600

    update telescope binding:

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -16,7 +16,7 @@ local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
   mapping = {
-    ["<C-Space>"] = cmp.mapping.confirm(),
+    ['<Tab>'] = cmp.mapping.confirm({select = true}),
     ["<C-f>"] = cmp_action.luasnip_jump_forward(),
     ["<C-b>"] = cmp_action.luasnip_jump_backward(),
   },
